20723856862ebb936c39e156a2342cbf00ea27d8
Muhammad Aiman bin Kanasronsham <96905225+aimanasrn@users.noreply.github.com>
2025-03-27 16:55:45 +0800

    attempt

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,18 @@
+import "dotenv/config";
+import mongoose from "mongoose";
 import express from "express";
 
 const app = express();
-const port = 5000;
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+  res.send("Hello World!");
 });
 
-app.listen(port, () => {
-    console.log("Server Runnning on port: "+ port)
-});
+const port = process.env.PORT;
+
+mongoose.connect(process.env.MONGO_CONNECTION_STRING!).then(() => {
+  console.log("mongoose connected");
+  app.listen(port, () => {
+    console.log("Server Runnning on port: " + port);
+  });
+}).catch(console.error)
